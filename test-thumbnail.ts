5097911c928e6a67e017647381903ab321fd5e4d
s12kuma01 <amukat1201.ta@gmail.com>
2025-10-16 00:56:46 +0900

    remove unnecessary files

--- test-thumbnail.ts
+++
@@ -1,4 +0,0 @@
-import { ThumbnailBuilder } from 'discord.js';
-
-const thumbnail = new ThumbnailBuilder();
-console.log('ThumbnailBuilder methods:', Object.getOwnPropertyNames(Object.getPrototypeOf(thumbnail)));
